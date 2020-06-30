637d91e50fe9739783d3d997f18f93769e7a1ed5
glasses-png <cchelangat16@gmail.com>
2020-06-30 15:06:48 +0300

    installed decouple

--- instaclone/settings.py
+++ instaclone/settings.py
@@ -13,7 +13,7 @@ SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-
+db_from_env = dj_database_url.config(conn_max_age=500)
 ALLOWED_HOSTS = 'chela-instagram.herokuapp.com'
 
 # Application definition
